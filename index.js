3a68e5c84c6de2f172e7ea3106d68c95b300cfae
Robin F <rfussell17@gmail.com>
2021-06-23 17:51:16 -0700

    add and style login, logout, home

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
 const express = require('express')
 const app = express();
+const path = require('path');
+const ejsMate = require('ejs-mate');
 
+app.engine('ejs', ejsMate)
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'))
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/', (req, res) => {
+    res.render('home')
+});
+
+app.get('/show', (req, res) => {
+    res.render('orders/show')
+});
+
+app.get('/login', (req, res) => {
+    res.render('users/login')
+});
+
+app.get('/register', (req, res) => {
+    res.render('users/register')
+});
+
+app.listen(3000, () => {
+    console.log('Serving on port 3000')
+})
